4fef3ae702e7f80a4ac602d90c2b1fa9977a8639
syn <vicdemilly@gmail.com>
2019-04-11 10:17:27 +0200

    Ajout de commentaires et autocorrection

---
+++ main.js
@@ -0,0 +1,6 @@
+function effacer()
+{
+  document.getElementById('infos').reset();
+}
+// L'idée est de réinitialiser toutes les valeurs écrites dans le formulaire
+// C'est le .reset à la fin de l'ID du formulaire qui le permet
